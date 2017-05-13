67ae33f14fc65cb334f93fbfa1871c135e3e7aa3
xemechen <xemechen@hotmail.com>
2017-05-13 14:22:48 +0800

    [alpha] SSH component update

--- scripts/SSH_Component.js
+++ scripts/SSH_Component.js
@@ -191,105 +191,117 @@ this.pageOperations = function(phInstanceParam, sitepage, dataPackage, waitFour,
         }, selector, index).then(function(processOutput){
         	if(processOutput != null && processOutput.length > 0){
         		console.log("Operation error: " + processOutput);	
-        	}                        	
+        	}else{
+                console.log("Operation finished: " + index);
+            }
         });
+    };
+
+    var ttst = function(){
+        return document.querySelectorAll("#Advertenties > a").length > 0;
     };
 
 	selecting("RegioDropDown", selectIndices[0] || 1);	
 	selecting("ContingenthouderDropDown", selectIndices[1] || 1);
 	selecting("ContingentDoelgroepDropDown", selectIndices[2] || 1);
-	selecting("ContingentPeriodeDropDown", selectIndices[3] || 1);
+    selecting("ContingentPeriodeDropDown", 0);
 
-	var ttst = function(){
-		return document.querySelectorAll("#Advertenties > a").length > 0;
-	};
+    var callingWaitFor = function(){	
+        waitFour(testing, ttst, function(){
+        	// callback function when page is loaded
+        	console.log("Calling onReady function");
+        	sitepage.evaluate(function(){
+            	var mapper = { "adres": "Address",
+                            "type": "Type",
+                            "grootte": "Size",
+                            "adressen": "Available",
+                            "prijs": "Price"
+                        };
+
+                var getChildrenEle = function(parent){
+                	if(parent == null){
+                		return [];
+                	}
+                    var children = parent.childNodes;
+                    var elementChild = [];
+                    for (i = 0; i < children.length; i++) {
+                        if(children[i].nodeType == 1){
+                            elementChild.push(children[i]);
+                        }
+                    }
+                    return elementChild;
+                }
+                var buildObjects = function(children){
+                	if(children.length == 0){
+                		return null;
+                	}
+
+                    var returnObj = {};
+                    for(var i = 0; i < children.length; i++){
+                        var clas = children[i].getAttribute("class")
+                        var text = children[i].textContent.split("\n").join("").trim();
+                        returnObj[mapper[clas]] = text;
+                    }
+                    return returnObj;
+                }
+
+                var returnList = [];
+                var returnObj = {'status':'success'};
+                try{
+                	if(document.querySelectorAll("#Advertenties").length == 0){
+                		returnObj.status = 'error';
+                		returnObj.error = "No results found";
+                		return JSON.stringify(returnObj);
+                	}
+                    var aList = document.querySelectorAll("#Advertenties > a");
+                    if(aList && aList.length > 0){
+                        for(var j = 0; j < aList.length; j++){
+                            var link = "https://booking.sshxl.nl" + aList[j].getAttribute("href");
+                            var children = getChildrenEle(aList[j].children[1]);
+                            var tempObj = buildObjects(children);
+                            if(tempObj == null){
+
+                            }else{
+                            	tempObj.link = link;
+                            	returnList.push(tempObj);	
+                            }                                        
+                        }
+                    }
+                }catch(err){
+                    console.log("Page fectching error: " + err);
+                    returnObj.error = err;
+                    returnObj.status = 'error';
+                }
+
+                returnObj.result = returnList;
+
+                return JSON.stringify(returnObj);
+            })
+            .then(function(outcome){  
+            	var outcomeObj = JSON.parse(outcome);
+            	if(outcomeObj.status == 'error'){
+        			console.log("Page fectching error: " + outcomeObj.error);
+            	}else{
+            		var callbackString = JSON.stringify(outcomeObj.result);
+            		console.log("Finished loading page");
+                	callback(callbackString);	
+            	}                            
+                phInstance.exit();
+
+                timeOutList = [];
+                var finalTO = setTimeout(function(){
+                	callback2();
+                }, delayMilSec);
+                timeOutList.push(finalTO);
+            });
+        }, 90000 + delayMilSec);
+    };
+
     waitFour(testing, ttst, function(){
-    	// callback function when page is loaded
-    	console.log("Calling onReady function");
-    	sitepage.evaluate(function(){
-        	var mapper = { "adres": "Address",
-                        "type": "Type",
-                        "grootte": "Size",
-                        "adressen": "Available",
-                        "prijs": "Price"
-                    };
-
-            var getChildrenEle = function(parent){
-            	if(parent == null){
-            		return [];
-            	}
-                var children = parent.childNodes;
-                var elementChild = [];
-                for (i = 0; i < children.length; i++) {
-                    if(children[i].nodeType == 1){
-                        elementChild.push(children[i]);
-                    }
-                }
-                return elementChild;
-            }
-            var buildObjects = function(children){
-            	if(children.length == 0){
-            		return null;
-            	}
-
-                var returnObj = {};
-                for(var i = 0; i < children.length; i++){
-                    var clas = children[i].getAttribute("class")
-                    var text = children[i].textContent.split("\n").join("").trim();
-                    returnObj[mapper[clas]] = text;
-                }
-                return returnObj;
-            }
-
-            var returnList = [];
-            var returnObj = {'status':'success'};
-            try{
-            	if(document.querySelectorAll("#Advertenties").length == 0){
-            		returnObj.status = 'error';
-            		returnObj.error = "No results found";
-            		return JSON.stringify(returnObj);
-            	}
-                var aList = document.querySelectorAll("#Advertenties > a");
-                if(aList && aList.length > 0){
-                    for(var j = 0; j < aList.length; j++){
-                        var link = "https://booking.sshxl.nl" + aList[j].getAttribute("href");
-                        var children = getChildrenEle(aList[j].children[1]);
-                        var tempObj = buildObjects(children);
-                        if(tempObj == null){
-
-                        }else{
-                        	tempObj.link = link;
-                        	returnList.push(tempObj);	
-                        }                                        
-                    }
-                }
-            }catch(err){
-                console.log("Page fectching error: " + err);
-                returnObj.error = err;
-                returnObj.status = 'error';
-            }
-
-            returnObj.result = returnList;
-
-            return JSON.stringify(returnObj);
-        })
-        .then(function(outcome){  
-        	var outcomeObj = JSON.parse(outcome);
-        	if(outcomeObj.status == 'error'){
-    			console.log("Page fectching error: " + outcomeObj.error);
-        	}else{
-        		var callbackString = JSON.stringify(outcomeObj.result);
-        		console.log("Finished loading page");
-            	callback(callbackString);	
-        	}                            
-            phInstance.exit();
-
-            timeOutList = [];
-            var finalTO = setTimeout(function(){
-            	callback2();
-            }, delayMilSec);
-            timeOutList.push(finalTO);
-        });
+        setTimeout(function(){
+            selecting("ContingentPeriodeDropDown", selectIndices[3] || 1);
+            setTimeout(callingWaitFor, 5000);
+        }, 5000);
     }, 90000 + delayMilSec);
 }
 
